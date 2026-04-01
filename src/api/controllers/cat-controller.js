cd0ec526c70b1baee5b83a6f9d004d9deeabf463
Fliits <aatuska576@gmail.com>
2026-04-01 11:17:00 +0300

    feat: implement database connection and CRUD operations for cats

--- src/api/controllers/cat-controller.js
+++ src/api/controllers/cat-controller.js
@@ -1,14 +1,11 @@
 import { addCat, findCatById, listAllCats } from "../models/cat-model.js";
 
-const getCat = (req, res) => {
-  //res.json(listAllCats());
-
-  console.log("get cat", req);
-  res.json({ ok: true, data: listAllCats() });
+const getCat = async (req, res) => {
+  res.json(await listAllCats());
 };
 
-const getCatById = (req, res) => {
-  const cat = findCatById(req.params.id);
+const getCatById = async (req, res) => {
+  const cat = await findCatById(req.params.id);
   if (cat) {
     res.json(cat);
   } else {
@@ -16,10 +13,10 @@ const getCatById = (req, res) => {
   }
 };
 
-const postCat = (req, res) => {
+const postCat = async (req, res) => {
   console.log("post cat", req.file);
   req.body.filename = req.file.filename;
-  const result = addCat(req.body);
+  const result = await addCat(req.body);
   if (result.cat_id) {
     res.status(201);
     res.json({ message: "New cat added.", result });
@@ -28,13 +25,13 @@ const postCat = (req, res) => {
   }
 };
 
-const putCat = (req, res) => {
+const putCat = async (req, res) => {
   // not implemented in this example, this is homework
   //res.sendStatus(200);
   res.json({ message: "Cat item updated." });
 };
 
-const deleteCat = (req, res) => {
+const deleteCat = async (req, res) => {
   // not implemented in this example, this is homework
   //res.sendStatus(200);
   res.json({ message: "Cat item deleted." });
--- src/api/models/cat-model.js
+++ src/api/models/cat-model.js
@@ -1,4 +1,5 @@
 // mock data
+
 const catItems = [
   {
     cat_id: 9592,
@@ -18,26 +19,62 @@ const catItems = [
   },
 ];
 
-const listAllCats = () => {
-  return catItems;
+import promisePool from "../../utils/database.js";
+
+const listAllCats = async () => {
+  const [rows] = await promisePool.query("SELECT * FROM wsk_cats");
+  console.log("rows", rows);
+  return rows;
 };
 
-const findCatById = (id) => {
-  return catItems.find((item) => item.cat_id == id);
+const findCatById = async (id) => {
+  const [rows] = await promisePool.execute(
+    "SELECT * FROM wsk_cats WHERE cat_id = ?",
+    [id],
+  );
+  console.log("rows", rows);
+  if (rows.length === 0) {
+    return false;
+  }
+  return rows[0];
 };
 
-const addCat = (cat) => {
+const addCat = async (cat) => {
   const { cat_name, weight, owner, filename, birthdate } = cat;
-  const newId = catItems[0].cat_id + 1;
-  catItems.unshift({
-    cat_id: newId,
-    cat_name,
-    weight,
-    owner,
-    filename,
-    birthdate,
-  });
-  return { cat_id: newId };
+  const sql = `INSERT INTO wsk_cats (cat_name, weight, owner, filename, birthdate)
+               VALUES (?, ?, ?, ?, ?)`;
+  const params = [cat_name, weight, owner, filename, birthdate];
+  const [result] = await promisePool.execute(sql, params);
+  console.log("result", result);
+  if (result.affectedRows === 0) {
+    return false;
+  }
+  return { cat_id: result.insertId };
 };
 
-export { listAllCats, findCatById, addCat };
+const modifyCat = async (cat, id) => {
+  const sql = promisePool.format(`UPDATE wsk_cats SET ? WHERE cat_id = ?`, [
+    cat,
+    id,
+  ]);
+  const [result] = await promisePool.execute(sql);
+  console.log("result", result);
+  if (result.affectedRows === 0) {
+    return false;
+  }
+  return { message: "success" };
+};
+
+const removeCat = async (id) => {
+  const [result] = await promisePool.execute(
+    "DELETE FROM wsk_cats WHERE cat_id = ?",
+    [id],
+  );
+  console.log("result", result);
+  if (result.affectedRows === 0) {
+    return false;
+  }
+  return { message: "success" };
+};
+
+export { listAllCats, findCatById, addCat, modifyCat, removeCat };
